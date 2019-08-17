5420e6fe135395e3e2ffb428c7e6ad9dd8b5ee3c
TopperDEL <github_com@tparth.de>
2019-08-17 21:51:31 +0200

    New working DLL, other test satellite

--- uplink.NET/uplink.NET.Test/TestConstants.cs
+++ uplink.NET/uplink.NET.Test/TestConstants.cs
@@ -6,7 +6,7 @@ namespace uplink.NET.Test
 {
     class TestConstants
     {
-        public const string VALID_API_KEY = "13YqeKQiA3ANSuDu4rqX6eGs3YWox9GRi9rEUKy1HidXiNNm6a5SiE49Hk9gomHZVcQhq4eFQh8yhDgfGKg268j6vqWKEhnJjFPLqAP";
+        public const string VALID_API_KEY = "13Yqe1K83MdaH7WJkR9qmsXaetDgQFzEiHmxCd3QMQfkZe1Hr2mYE8sbaoG6f74tYGQ7QmMt2bdaCJJy8dXLgJNyKKfzkeWkLa4ib9H";
         public const string INVALID_API_KEY = "This is not a valid API-Key";
     }
 }
