32a43eca5ba95de84602ade415e7f59df671e376
TopperDEL <github_com@tparth.de>
2019-08-23 13:15:13 +0200

    Include Android-Project and build artifacts

---
+++ uplink.NET/uplink.NET.Android/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("uplink.NET.Android")]
+[assembly: AssemblyDescription("Provides access to the storj-network")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TopperDEL")]
+[assembly: AssemblyProduct("uplink.NET.Android")]
+[assembly: AssemblyCopyright("Copyright TopperDEL © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
