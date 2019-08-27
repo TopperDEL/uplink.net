6a41b0fde012aef2e9c035c4423590f033594a79
TopperDEL <github_com@tparth.de>
2019-08-27 10:17:30 +0200

    Better enum-handling of RedundancyAlgorithm and CipherSuite

--- uplink.NET/uplink.NET.Shared/Models/EncryptionParameters.cs
+++ uplink.NET/uplink.NET.Shared/Models/EncryptionParameters.cs
@@ -6,14 +6,14 @@ namespace uplink.NET.Models
 {
     public class EncryptionParameters
     {
-        public int CipherSuite { get; set; }
+        public CipherSuite CipherSuite { get; set; }
         public int BlockSize { get; set; }
 
         internal static EncryptionParameters FromSWIG(SWIG.EncryptionParameters original)
         {
             EncryptionParameters ret = new EncryptionParameters();
             ret.BlockSize = original.block_size;
-            //ToDo: do mapping - ret.CipherSuite = original.cipher_suite;
+            ret.CipherSuite = CipherSuiteHelper.FromSWIG(original.cipher_suite);
 
             return ret;
         }
