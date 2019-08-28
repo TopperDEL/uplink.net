16d0640f5597ce3a03e34ae61666d3a36170d936
TopperDEL <github_com@tparth.de>
2019-08-28 08:30:42 +0200

    ObjectService-implementation alpha

---
+++ uplink.NET/uplink.NET.Shared/Services/ObjectService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using uplink.NET.Interfaces;
+using uplink.NET.Models;
+
+namespace uplink.NET.Services
+{
+    public class ObjectService : IObjectService
+    {
+        public void UploadObject(BucketRef bucket, string targetPath, UploadOptions uploadOptions)
+        {
+            string error;
+
+            var uploadRef = SWIG.storj_uplink.upload(bucket._bucketRef, targetPath, uploadOptions._handle, out error);
+            byte[] stringBytes = Encoding.UTF8.GetBytes("Mein erster upload");
+            uint length = (uint)stringBytes.Length;
+            var returnedLength = SWIG.storj_uplink.upload_write(uploadRef, stringBytes, length, out error);
+        }
+    }
+}
