fe2389973d747643712b2d4201d3f552f00ac4ea
TopperDEL <github_com@tparth.de>
2019-08-28 08:30:23 +0200

    First ObjectService-implementation

---
+++ uplink.NET/uplink.NET.Shared/Models/UploadOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uplink.NET.Models
+{
+    public class UploadOptions
+    {
+        internal SWIG.UploadOptions _handle;
+    }
+}
