77e34ee021aa80d1290666bbead3b4d3d2bfa648
TopperDEL <github_com@tparth.de>
2019-08-28 08:30:23 +0200

    First ObjectService-implementation

---
+++ uplink.NET/uplink.NET.Shared/Interfaces/IObjectService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using uplink.NET.Models;
+
+namespace uplink.NET.Interfaces
+{
+    public interface IObjectService
+    {
+        void UploadObject(BucketRef bucket, string targetPath, UploadOptions uploadOptions);
+    }
+}
---
+++ uplink.NET/uplink.NET.Test/ObjectServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using uplink.NET.Interfaces;
+using uplink.NET.Models;
+using uplink.NET.Services;
+
+namespace uplink.NET.Test
+{
+    [TestClass]
+
+    public class ObjectServiceTest
+    {
+        IBucketService _bucketService;
+        IObjectService _objectService;
+        Project _project;
+        BucketConfig _bucketConfig;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _bucketService = new BucketService();
+            _objectService = new ObjectService();
+            UplinkConfig config = new UplinkConfig();
+            Uplink uplink = new Uplink(config);
+            ApiKey apiKey = new ApiKey(TestConstants.VALID_API_KEY);
+            ProjectOptions projectOptions = new ProjectOptions();
+            _project = new Project(uplink, apiKey, TestConstants.SATELLITE_URL, projectOptions);
+            _bucketConfig = new BucketConfig();
+        }
+
+        [TestMethod]
+        public void UploadObjectTest()
+        {
+            string bucketname = "uploadtest";
+
+            var result = _bucketService.CreateBucket(_project, bucketname, _bucketConfig);
+            var bucket = _bucketService.OpenBucket(_project, bucketname, EncryptionAccess.FromPassphrase(_project, TestConstants.ENCRYPTION_SECRET));
+            _objectService.UploadObject(bucket, "myfile.txt", new UploadOptions());
+
+            Assert.AreEqual(bucketname, result.Name);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            DeleteBucket("uploadtest");
+        }
+
+        private void DeleteBucket(string bucketName)
+        {
+            try
+            {
+                _bucketService.DeleteBucket(_project, bucketName);
+            }
+            catch
+            { }
+        }
+    }
+}
