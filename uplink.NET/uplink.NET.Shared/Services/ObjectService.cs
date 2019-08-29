d4a91483075e33b250e414468eb929f484d9d39e
TopperDEL <github_com@tparth.de>
2019-08-29 15:16:20 +0200

    Temporary Upload-Download-Test

--- uplink.NET/uplink.NET.Shared/Services/ObjectService.cs
+++ uplink.NET/uplink.NET.Shared/Services/ObjectService.cs
@@ -16,6 +16,13 @@ namespace uplink.NET.Services
             byte[] stringBytes = Encoding.UTF8.GetBytes("Mein erster upload");
             uint length = (uint)stringBytes.Length;
             var returnedLength = SWIG.storj_uplink.upload_write(uploadRef, stringBytes, length, out error);
+            SWIG.storj_uplink.upload_commit(uploadRef, out error);
+
+            var downloadRef = SWIG.storj_uplink.download(bucket._bucketRef, targetPath, out error);
+            byte[] readBytes = new byte[length];
+            SWIG.storj_uplink.download_read(downloadRef, readBytes, length, out error);
+            SWIG.storj_uplink.download_close(downloadRef, out error);
+            string resultString = Encoding.UTF8.GetString(readBytes);
         }
     }
 }
