b160259739e0f3ab021697b5d3bc0f98a9d9295a
TopperDEL <github_com@tparth.de>
2019-08-29 16:25:36 +0200

    Async UploadOperation with tests

--- uplink.NET/uplink.NET.Shared/Services/ObjectService.cs
+++ uplink.NET/uplink.NET.Shared/Services/ObjectService.cs
@@ -8,21 +8,17 @@ namespace uplink.NET.Services
 {
     public class ObjectService : IObjectService
     {
-        public void UploadObject(BucketRef bucket, string targetPath, UploadOptions uploadOptions)
+        public UploadOperation UploadObject(BucketRef bucket, string targetPath, UploadOptions uploadOptions, byte[] bytesToUpload, bool immediateStart = true)
         {
             string error;
 
-            var uploadRef = SWIG.storj_uplink.upload(bucket._bucketRef, targetPath, uploadOptions._handle, out error);
-            byte[] stringBytes = Encoding.UTF8.GetBytes("Mein erster upload");
-            uint length = (uint)stringBytes.Length;
-            var returnedLength = SWIG.storj_uplink.upload_write(uploadRef, stringBytes, length, out error);
-            SWIG.storj_uplink.upload_commit(uploadRef, out error);
+            var uploaderRef = SWIG.storj_uplink.upload(bucket._bucketRef, targetPath, uploadOptions.ToSWIG(), out error);
 
-            var downloadRef = SWIG.storj_uplink.download(bucket._bucketRef, targetPath, out error);
-            byte[] readBytes = new byte[length];
-            SWIG.storj_uplink.download_read(downloadRef, readBytes, length, out error);
-            SWIG.storj_uplink.download_close(downloadRef, out error);
-            string resultString = Encoding.UTF8.GetString(readBytes);
+            UploadOperation upload = new UploadOperation(bytesToUpload, uploaderRef);
+            if(immediateStart)
+                upload.StartUploadAsync();
+
+            return upload;
         }
     }
 }
