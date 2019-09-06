f541e233d9b40dee0ef01723719b12fc31a1fafa
TopperDEL <github_com@tparth.de>
2019-09-06 15:11:52 +0200

    LoginPage with functionality

---
+++ uplink.NET/uplink.NET.Sample/uplink.NET.Sample.Shared/Commands/LoginCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using uplink.NET.Sample.Shared.Interfaces;
+using uplink.NET.Sample.Shared.Pages;
+using uplink.NET.Sample.Shared.ViewModels;
+
+namespace uplink.NET.Sample.Shared.Commands
+{
+    public class LoginCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private ILoginService _loginService;
+
+        public LoginCommand(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            LoginViewModel viewModel = parameter as LoginViewModel;
+
+            var loggedIn = _loginService.Login(viewModel.LoginData);
+
+            if (loggedIn)
+            {
+                var frame = (Windows.UI.Xaml.Controls.Frame)Windows.UI.Xaml.Window.Current.Content;
+                frame.Navigate(typeof(MainPage));
+            }
+        }
+    }
+}
---
+++ uplink.NET/uplink.NET.Sample/uplink.NET.Sample.Shared/Services/Factory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using uplink.NET.Sample.Shared.Interfaces;
+
+namespace uplink.NET.Sample.Shared.Services
+{
+    public static class Factory
+    {
+        private static ILoginService _loginService;
+        public static ILoginService LoginService
+        {
+            get
+            {
+                if (_loginService == null)
+                    _loginService = new LoginService();
+                return _loginService;
+            }
+        }
+    }
+}
