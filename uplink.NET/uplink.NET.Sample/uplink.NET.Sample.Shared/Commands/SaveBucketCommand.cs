9f9874978ba49aafb2225bdf1a4ab5a12bd80228
TopperDEL <github_com@tparth.de>
2019-09-08 21:18:36 +0200

    CreateBucket works from SampleApp

---
+++ uplink.NET/uplink.NET.Sample/uplink.NET.Sample.Shared/Commands/SaveBucketCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using uplink.NET.Interfaces;
+using uplink.NET.Sample.Shared.Interfaces;
+using uplink.NET.Sample.Shared.Pages;
+using uplink.NET.Sample.Shared.Services;
+using uplink.NET.Sample.Shared.ViewModels;
+
+namespace uplink.NET.Sample.Shared.Commands
+{
+    public class SaveBucketCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        IBucketService _bucketService;
+        IStorjService _storjService;
+
+        public SaveBucketCommand(IBucketService bucketService, IStorjService storjService)
+        {
+            _bucketService = bucketService;
+            _storjService = storjService;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            CreateBucketViewModel createBucketViewModel = parameter as CreateBucketViewModel;
+
+            try
+            {
+                await _bucketService.CreateBucketAsync(_storjService.Project, createBucketViewModel.BucketName, new NET.Models.BucketConfig());
+            }
+            catch (Exception ex)
+            {
+                Windows.UI.Popups.MessageDialog dialog = new Windows.UI.Popups.MessageDialog("Could not create bucket - " + ex.Message);
+                await dialog.ShowAsync();
+                return;
+            }
+            var frame = (Windows.UI.Xaml.Controls.Frame)Windows.UI.Xaml.Window.Current.Content;
+            frame.Navigate(typeof(BucketListPage));
+        }
+    }
+}
---
+++ uplink.NET/uplink.NET.Sample/uplink.NET.Sample.Shared/ViewModels/CreateBucketViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using uplink.NET.Interfaces;
+using uplink.NET.Sample.Shared.Commands;
+using uplink.NET.Sample.Shared.Interfaces;
+
+namespace uplink.NET.Sample.Shared.ViewModels
+{
+    public class CreateBucketViewModel:BaseViewModel
+    {
+        public string BucketName { get; set; }
+        public ICommand SaveBucketCommand { get; set; }
+
+        public CreateBucketViewModel(IBucketService bucketService, IStorjService storjService)
+        {
+            SaveBucketCommand = new SaveBucketCommand(bucketService, storjService);
+        }
+    }
+}
