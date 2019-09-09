4e4e34dd180a1a3a7435fa4cee15139501ece73e
TopperDEL <github_com@tparth.de>
2019-09-09 16:39:27 +0200

    First version of OpenBucket in SampleApp

---
+++ uplink.NET/uplink.NET.Sample/uplink.NET.Sample.Shared/ViewModels/BucketContentViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using uplink.NET.Interfaces;
+using uplink.NET.Models;
+using uplink.NET.Sample.Shared.Interfaces;
+using System.Linq;
+
+namespace uplink.NET.Sample.Shared.ViewModels
+{
+    public class BucketContentViewModel : BaseViewModel
+    {
+        public static Dictionary<string, UploadOperation> ActiveUploadOperations = new Dictionary<string, UploadOperation>();
+        public ObservableCollection<BucketEntryViewModel> Entries { get; set; }
+        public string BucketName { get; set; }
+
+        IObjectService _objectService;
+        IBucketService _bucketService;
+        IStorjService _storjService;
+        ILoginService _loginService;
+
+        public BucketContentViewModel(IObjectService objectService, IBucketService bucketService, IStorjService storjService, ILoginService loginService, string bucketName)
+        {
+            Entries = new ObservableCollection<BucketEntryViewModel>();
+
+            _objectService = objectService;
+            _bucketService = bucketService;
+            _storjService = storjService;
+            _loginService = loginService;
+            BucketName = bucketName;
+
+            InitAsync();
+        }
+
+        private async Task InitAsync()
+        {
+            //Load all options
+            try
+            {
+                var bucket = await _bucketService.OpenBucketAsync(_storjService.Project, BucketName, EncryptionAccess.FromPassphrase(_storjService.Project, _loginService.GetLoginData().Secret));
+                var listOptions = new ListOptions();
+                listOptions.Direction = ListDirection.STORJ_FORWARD;
+                var objects = await _objectService.ListObjectsAsync(bucket, listOptions);
+                foreach (var obj in objects.Items)
+                {
+                    var entry = new BucketEntryViewModel();
+                    entry.IsObject = true;
+                    entry.ObjectInfo = obj;
+                    Entries.Add(entry);
+                }
+            }
+            catch(Exception ex)
+            {
+                Windows.UI.Popups.MessageDialog dialog = new Windows.UI.Popups.MessageDialog("Could not open bucket - " + ex.Message);
+                await dialog.ShowAsync();
+            }
+
+            //Fetch all UploadOperations
+            foreach(var uploadOperation in ActiveUploadOperations.Where(u=>u.Key == BucketName))
+            {
+                var entry = new BucketEntryViewModel();
+                entry.IsUploadOperation = true;
+                entry.UploadOperation = uploadOperation.Value;
+                Entries.Add(entry);
+            }
+
+            DoneLoading();
+        }
+    }
+}
---
+++ uplink.NET/uplink.NET.Sample/uplink.NET.Sample.Shared/ViewModels/BucketEntryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using uplink.NET.Models;
+
+namespace uplink.NET.Sample.Shared.ViewModels
+{
+    public class BucketEntryViewModel : BaseViewModel
+    {
+        public bool IsUploadOperation { get; set; }
+        public UploadOperation UploadOperation { get; set; }
+
+        public bool IsObject { get; set; }
+        public ObjectInfo ObjectInfo { get; set; }
+
+        public bool IsDownload { get; set; }
+        public DownloadOperation DownloadOperation { get; set; }
+    }
+}
