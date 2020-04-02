c3f17ddf9820ac273193d6d5696f008f6b767a40
TopperDEL <github_com@tparth.de>
2020-04-02 13:09:35 +0200

    feat: correct disposal of bucket and bucket_result

--- uplink.NET/uplink.NET.Shared/Models/Bucket.cs
+++ uplink.NET/uplink.NET.Shared/Models/Bucket.cs
@@ -7,9 +7,10 @@ namespace uplink.NET.Models
     /// <summary>
     /// The handle for an opened bucket. The bucket has to be closed with this handle after use.
     /// </summary>
-    public class Bucket
+    public class Bucket: IDisposable
     {
         internal SWIG.Bucket _bucketRef;
+        internal SWIG.BucketResult _bucketResultRef;
 
         public string Name
         {
@@ -32,12 +33,29 @@ namespace uplink.NET.Models
 
         }
 
-        internal static Bucket FromSWIG(SWIG.Bucket original)
+        internal static Bucket FromSWIG(SWIG.Bucket original, SWIG.BucketResult bucketResult = null)
         {
             Bucket ret = new Bucket();
             ret._bucketRef = original;
+            ret._bucketResultRef = bucketResult;
 
             return ret;
+        }
+
+        public void Dispose()
+        {
+            if(_bucketRef != null)
+            {
+                SWIG.storj_uplink.free_bucket(_bucketRef);
+                _bucketRef.Dispose();
+                _bucketRef = null;
+            }
+            if (_bucketResultRef != null)
+            {
+                SWIG.storj_uplink.free_bucket_result(_bucketResultRef);
+                _bucketResultRef.Dispose();
+                _bucketResultRef = null;
+            }
         }
     }
 }
--- uplink.NET/uplink.NET.Shared/Services/BucketService.cs
+++ uplink.NET/uplink.NET.Shared/Services/BucketService.cs
@@ -19,49 +19,74 @@ namespace uplink.NET.Services
 
         public async Task<Bucket> CreateBucketAsync(string bucketName)
         {
-            SWIG.BucketResult bucketResult = await Task.Run(() => SWIG.storj_uplink.create_bucket(_access._project, bucketName));
+            using (SWIG.BucketResult bucketResult = await Task.Run(() => SWIG.storj_uplink.create_bucket(_access._project, bucketName)))
+            {
+                if (bucketResult.error != null && !string.IsNullOrEmpty(bucketResult.error.message))
+                    throw new BucketCreationException(bucketName, bucketResult.error.message);
 
-            if (bucketResult.error != null && !string.IsNullOrEmpty(bucketResult.error.message))
-                throw new BucketCreationException(bucketName, bucketResult.error.message);
+                var bucket = Bucket.FromSWIG(bucketResult.bucket, bucketResult);
 
-            return Bucket.FromSWIG(bucketResult.bucket);
+                return bucket;
+            }
+        }
+
+        public async Task<Bucket> EnsureBucketAsync(string bucketName)
+        {
+            using (SWIG.BucketResult bucketResult = await Task.Run(() => SWIG.storj_uplink.ensure_bucket(_access._project, bucketName)))
+            {
+                if (bucketResult.error != null && !string.IsNullOrEmpty(bucketResult.error.message))
+                    throw new BucketCreationException(bucketName, bucketResult.error.message);
+
+                var bucket = Bucket.FromSWIG(bucketResult.bucket, bucketResult);
+
+                return bucket;
+            }
         }
 
         public async Task DeleteBucketAsync(string bucketName)
         {
-            SWIG.BucketResult bucketResult = await Task.Run(() => SWIG.storj_uplink.delete_bucket(_access._project, bucketName));
+            using (SWIG.BucketResult bucketResult = await Task.Run(() => SWIG.storj_uplink.delete_bucket(_access._project, bucketName)))
+            {
+                if (bucketResult.error != null && !string.IsNullOrEmpty(bucketResult.error.message))
+                    throw new BucketDeletionException(bucketName, bucketResult.error.message);
 
-            if (bucketResult.error != null && !string.IsNullOrEmpty(bucketResult.error.message))
-                throw new BucketDeletionException(bucketName, bucketResult.error.message);
+                SWIG.storj_uplink.free_bucket_result(bucketResult);
+            }
         }
 
         public async Task<Bucket> GetBucketAsync(string bucketName)
         {
-            SWIG.BucketResult bucketResult = await Task.Run(() => SWIG.storj_uplink.stat_bucket(_access._project, bucketName));
+            using (SWIG.BucketResult bucketResult = await Task.Run(() => SWIG.storj_uplink.stat_bucket(_access._project, bucketName)))
+            {
+                if (bucketResult.error != null && !string.IsNullOrEmpty(bucketResult.error.message))
+                    throw new BucketNotFoundException(bucketName, bucketResult.error.message);
 
-            if (bucketResult.error != null && !string.IsNullOrEmpty(bucketResult.error.message))
-                throw new BucketNotFoundException(bucketName, bucketResult.error.message);
+                var bucket = Bucket.FromSWIG(bucketResult.bucket, bucketResult);
 
-            return Bucket.FromSWIG(bucketResult.bucket);
+                return bucket;
+            }
         }
 
         public async Task<BucketList> ListBucketsAsync(ListBucketsOptions listBucketsOptions)
         {
-            SWIG.BucketIterator bucketIterator = await Task.Run(() => SWIG.storj_uplink.list_buckets(_access._project, listBucketsOptions.ToSWIG()));
-
-            var error = SWIG.storj_uplink.bucket_iterator_err(bucketIterator);
-            if (error != null && !string.IsNullOrEmpty(error.message))
-                throw new BucketListException(error.message);
-
-            BucketList bucketList = new BucketList();
-
-            while(SWIG.storj_uplink.bucket_iterator_next(bucketIterator))
+            using (SWIG.BucketIterator bucketIterator = await Task.Run(() => SWIG.storj_uplink.list_buckets(_access._project, listBucketsOptions.ToSWIG())))
             {
-                bucketList.Items.Add(Bucket.FromSWIG(SWIG.storj_uplink.bucket_iterator_item(bucketIterator)));
-            }
-            SWIG.storj_uplink.free_bucket_iterator(bucketIterator);
 
-            return bucketList;
+                var error = SWIG.storj_uplink.bucket_iterator_err(bucketIterator);
+                if (error != null && !string.IsNullOrEmpty(error.message))
+                    throw new BucketListException(error.message);
+
+                BucketList bucketList = new BucketList();
+
+                while (SWIG.storj_uplink.bucket_iterator_next(bucketIterator))
+                {
+                    var bucket = SWIG.storj_uplink.bucket_iterator_item(bucketIterator);
+                    bucketList.Items.Add(Bucket.FromSWIG(bucket));
+                }
+                SWIG.storj_uplink.free_bucket_iterator(bucketIterator);
+
+                return bucketList;
+            }
         }
     }
 }
