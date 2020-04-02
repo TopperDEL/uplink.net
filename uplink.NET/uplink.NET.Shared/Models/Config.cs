53374ae4ba1c75f2e43e528e88fd65639f83d82c
TopperDEL <github_com@tparth.de>
2020-04-02 13:15:27 +0200

    feat: add request_access with config

---
+++ uplink.NET/uplink.NET.Shared/Models/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uplink.NET.Models
+{
+    public class Config
+    {
+        public string UserAgent { get; set; }
+        public int DialTimeoutMilliseconds { get; set; }
+        public string TempDirectory { get; set; }
+
+        internal SWIG.Config ToSWIG()
+        {
+            SWIG.Config config = new SWIG.Config();
+            config.dial_timeout_milliseconds = DialTimeoutMilliseconds;
+            config.temp_directory = TempDirectory;
+            config.user_agent = UserAgent;
+
+            return config;
+        }
+    }
+}
