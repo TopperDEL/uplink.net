7edf437c88497262dce5a8c9213f601025ac6e88
TopperDEL <github_com@tparth.de>
2021-01-29 15:17:37 +0100

    feat: Rework sample-app

---
+++ uplink.NET/uplink.NET.Sample/uplink.NET.Sample.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace uplink.NET.Sample.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new uplink.NET.Sample.App(), args);
+            host.Run();
+        }
+    }
+}
