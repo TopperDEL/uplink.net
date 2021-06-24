7f6726b44701a2704ce0083feb34a3b5e0936265
TopperDEL <github_com@tparth.de>
2021-06-24 21:16:13 +0200

    feat: Multipart-Upload WIP

--- uplink.NET/uplink.NET/Services/UploadQueueService.cs
+++ uplink.NET/uplink.NET/Services/UploadQueueService.cs
@@ -15,13 +15,24 @@ namespace uplink.NET.Services
     public class UploadQueueService : IUploadQueueService
     {
         SQLiteAsyncConnection _connection;
-        CancellationToken _uploadCancelToken;
+        CancellationTokenSource _source;
         Task _uploadTask;
         readonly string _databasePath;
 
         public UploadQueueService()
         {
             _databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "uplinkNET.db");
+        }
+
+        public async Task ClearAllPendingUploadsAsync()
+        {
+            await InitAsync();
+
+            await _connection.DropTableAsync<UploadQueueEntry>();
+            await _connection.DropTableAsync<UploadQueueEntryData>();
+
+            await _connection.CloseAsync();
+            _connection = null;
         }
 
         public UploadQueueService(string databasePath)
@@ -33,7 +44,7 @@ namespace uplink.NET.Services
         {
             get
             {
-                return _uploadTask != null && (_uploadTask.Status == TaskStatus.Running || _uploadTask.Status == TaskStatus.WaitingToRun);
+                return _uploadTask != null && (_uploadTask.Status == TaskStatus.Running || _uploadTask.Status == TaskStatus.WaitingToRun || _uploadTask.Status == TaskStatus.WaitingForActivation);
             }
         }
 
@@ -44,28 +55,44 @@ namespace uplink.NET.Services
                 _connection = new SQLiteAsyncConnection(_databasePath);
 
                 await _connection.CreateTableAsync<UploadQueueEntry>();
+                await _connection.CreateTableAsync<UploadQueueEntryData>();
             }
         }
 
-        public async Task AddObjectToUploadQueue(string bucketName, string key, string accessGrant, byte[] objectData, string identifier = null)
+        public async Task AddObjectToUploadQueue(string bucketName, string key, string accessGrant, byte[] objectData, string identifier)
         {
             await InitAsync();
 
             var entry = new UploadQueueEntry();
             entry.AccessGrant = accessGrant;
-            entry.Identifier = string.IsNullOrWhiteSpace(identifier) ? Guid.NewGuid().ToString() : identifier;
+            entry.Identifier = identifier;
             entry.BucketName = bucketName;
             entry.Key = key;
-            entry.Bytes = objectData;
+            entry.TotalBytes = objectData.Length;
+            entry.BytesCompleted = 0;
 
             await _connection.InsertAsync(entry);
+
+            var entryData = new UploadQueueEntryData();
+            entryData.UploadQueueEntryId = entry.Id;
+            entryData.Bytes = objectData;
+
+            await _connection.InsertAsync(entryData);
+
+            ProcessQueueInBackground();
         }
 
         public async Task CancelUploadAsync(string key)
         {
             await InitAsync();
 
-            await _connection.Table<UploadQueueEntry>().DeleteAsync(e => e.Key == key);
+            var entry = await _connection.Table<UploadQueueEntry>().Where(e => e.Key == key).FirstOrDefaultAsync();
+            if (entry != null)
+            {
+                var id = entry.Id;
+                await _connection.Table<UploadQueueEntry>().DeleteAsync(e => e.Id == id);
+                await _connection.Table<UploadQueueEntryData>().DeleteAsync(e => e.UploadQueueEntryId == id);
+            }
         }
 
         public Task<List<UploadOperation>> GetAwaitingUploadsAsync()
@@ -75,15 +102,20 @@ namespace uplink.NET.Services
 
         public void ProcessQueueInBackground()
         {
-            if (_uploadTask == null || _uploadTask.Status != TaskStatus.Running)
+            if (_uploadTask == null)
             {
-                if (_uploadTask != null)
-                    _uploadTask.Dispose();
+                _source = new CancellationTokenSource();
+                var uploadCancelToken = _source.Token;
 
-                CancellationTokenSource source = new CancellationTokenSource();
-                _uploadCancelToken = source.Token;
+                _uploadTask = Task.Run(() => DoUploadAsync(uploadCancelToken), uploadCancelToken);
+            }
+        }
 
-                _uploadTask = Task.Factory.StartNew(() => DoUploadAsync(_uploadCancelToken), _uploadCancelToken, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        public void StopQueueInBackground()
+        {
+            if(_source!= null && _source.Token.CanBeCanceled)
+            {
+                _source.Cancel();
             }
         }
 
@@ -95,32 +127,82 @@ namespace uplink.NET.Services
             {
                 while (!token.IsCancellationRequested)
                 {
-                    var toUpload = await _connection.Table<UploadQueueEntry>().FirstAsync();
-
-                    try
+                    var toUpload = await _connection.Table<UploadQueueEntry>().Where(e => !e.Failed).FirstOrDefaultAsync();
+                    if (toUpload != null)
                     {
-                        var access = new Access(toUpload.AccessGrant);
-                        var bucketService = new BucketService(access);
-                        var bucket = await bucketService.GetBucketAsync(toUpload.BucketName);
-                        var objectService = new ObjectService(access);
-                        var upload = await objectService.UploadObjectAsync(bucket, toUpload.Key, new UploadOptions(), toUpload.Bytes, false);
-                        await upload.StartUploadAsync();
+                        try
+                        {
+                            var access = new Access(toUpload.AccessGrant);
+                            var bucketService = new BucketService(access);
+                            var bucket = await bucketService.GetBucketAsync(toUpload.BucketName);
+                            var multipartUploadService = new MultipartUploadService(access);
 
-                        if (upload.Completed)
-                        {
-                            await _connection.Table<UploadQueueEntry>().DeleteAsync(e => e.Id == toUpload.Id);
+                            //If the upload has not UploadId, begin it
+                            if (!token.IsCancellationRequested && string.IsNullOrEmpty(toUpload.UploadId))
+                            {
+                                var uploadInfo = await multipartUploadService.BeginUploadAsync(toUpload.BucketName, toUpload.Key, new UploadOptions());
+                                toUpload.UploadId = uploadInfo.UploadId;
+
+                                //Save the UploadId
+                                await _connection.UpdateAsync(toUpload);
+                            }
+
+                            if (!token.IsCancellationRequested)
+                            {
+                                var toUploadData = await _connection.Table<UploadQueueEntryData>().Where(d => d.UploadQueueEntryId == toUpload.Id).FirstOrDefaultAsync();
+                                if (toUploadData != null)
+                                {
+                                    while (!token.IsCancellationRequested && toUpload.BytesCompleted != toUpload.TotalBytes)
+                                    {
+                                        //Now upload batches of 256 KiB (262144 bytes)
+                                        var bytesToUpload = toUploadData.Bytes.Skip(toUpload.BytesCompleted).Take(262144).ToArray();
+                                        var upload = await multipartUploadService.UploadPartAsync(toUpload.BucketName, toUpload.Key, toUpload.UploadId, toUpload.CurrentPartNumber, bytesToUpload);
+
+                                        //Refresh the uploaded bytes counter and define the next part number
+                                        toUpload.BytesCompleted += (int)upload.BytesWritten;
+                                        toUpload.CurrentPartNumber++;
+
+                                        //Save the current state
+                                        await _connection.UpdateAsync(toUpload);
+                                    }
+                                }
+                            }
+
+                            //If all bytes are uploaded, commit the upload.
+                            if (!token.IsCancellationRequested && toUpload.BytesCompleted == toUpload.TotalBytes)
+                            {
+                                var commitResult = await multipartUploadService.CommitUploadAsync(toUpload.BucketName, toUpload.Key, toUpload.UploadId, new CommitUploadOptions());
+                                if (string.IsNullOrEmpty(commitResult.Error))
+                                {
+                                    await _connection.Table<UploadQueueEntry>().DeleteAsync(e => e.Id == toUpload.Id);
+                                    await _connection.Table<UploadQueueEntryData>().DeleteAsync(e => e.UploadQueueEntryId == toUpload.Id);
+                                }
+                                else
+                                {
+                                    toUpload.Failed = true;
+                                    toUpload.FailedMessage = commitResult.Error;
+
+                                    //Save the current state
+                                    await _connection.UpdateAsync(toUpload);
+                                }
+                            }
                         }
-                        else
+                        catch (Exception ex)
                         {
-                            //ToDo: Error handling
+                            toUpload.Failed = true;
+                            toUpload.FailedMessage = ex.Message;
+
+                            //Save the current state
+                            await _connection.UpdateAsync(toUpload);
                         }
                     }
-                    catch (Exception ex)
+                    else
                     {
+                        return;
                     }
                 }
             }
-            catch { } //That's ok, nothing more to do.
+            catch { } //That's ok, simply quit. The next run should fix it.
         }
 
         public async Task<int> GetOpenUploadCountAsync()
