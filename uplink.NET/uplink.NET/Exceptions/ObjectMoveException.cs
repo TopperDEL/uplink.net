5c851e9c3b99a152dc311d520d4aa3b0b250dcb3
TopperDEL <github_com@tparth.de>
2022-01-18 14:25:49 +0100

    feat: MoveObject

---
+++ uplink.NET/uplink.NET/Exceptions/ObjectMoveException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uplink.NET.Exceptions
+{
+    /// <summary>
+    /// An object could not be moved
+    /// </summary>
+    public class ObjectMoveException : Exception
+    {
+        public ObjectMoveException(string error):base(error)
+        {
+
+        }
+    }
+}
