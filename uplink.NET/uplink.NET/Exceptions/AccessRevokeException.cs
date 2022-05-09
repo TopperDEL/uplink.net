9ec9ed0cb27e5b9dd028e9ddf74b9e37c0c4f9d8
TopperDEL <github_com@tparth.de>
2022-05-09 14:52:25 +0200

    feat: revoke access

---
+++ uplink.NET/uplink.NET/Exceptions/AccessRevokeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uplink.NET.Exceptions
+{
+    /// <summary>
+    /// The Access could not be revoked
+    /// </summary>
+    public class AccessRevokeException : Exception
+    {
+        public AccessRevokeException(string error):base(error)
+        {
+
+        }
+    }
+}
