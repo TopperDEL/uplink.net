f8def8605306f311a69ad6c000c042b9331bc9e3
TopperDEL <github_com@tparth.de>
2022-05-09 15:17:05 +0200

    feat: Replace ArgumentException with more specific ones

---
+++ uplink.NET/uplink.NET/Exceptions/AccessException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uplink.NET.Exceptions
+{
+    /// <summary>
+    /// An access was not successfull
+    /// </summary>
+    public class AccessException : Exception
+    {
+        public AccessException(string error):base(error)
+        {
+
+        }
+    }
+}
