44faf414f13e3a4cfad1e3862a339854e7e6ec84
TopperDEL <github_com@tparth.de>
2023-12-21 09:59:19 +0100

    fix: Remove secret - API-key already revoked

--- uplink.NET/uplink.NET.Test/TestConstants.cs
+++ uplink.NET/uplink.NET.Test/TestConstants.cs
@@ -6,7 +6,7 @@ namespace uplink.NET.Test
 {
     class TestConstants
     {
-        public const string VALID_API_KEY = "1FkahBKy3PeTFzpgntwEubptFb7j7QGY7SW1F89Y9BHynmvKoPTecteVQoPjF3dBpLv1Rf6ZCruNWkK1MHii1i2pEMij9yG9Q9PwgxK1nLcFmDBH7FpLDAfkSAffvVTtJLbssVw37gNdrfAvx1ZJW";
+        public const string VALID_API_KEY = "your-API-key-here";
         public const string ENCRYPTION_SECRET = "i_am_so_secret_you_cant_imagine";
         public const string INVALID_API_KEY = "This is not a valid API-Key";
         public const string SATELLITE_URL = "europe-west-1.tardigrade.io:7777"; //Use the address of your satellite
